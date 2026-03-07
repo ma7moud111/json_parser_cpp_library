1707a38dd15514dcbd7dd1ded4cf779533199f87
Mahmoud Sayed <ma7moudsayed111@gmail.com>
2026-03-07 19:54:10 +0200

    refactor: implement json value class methods

--- include/json_value.hpp
+++ include/json_value.hpp
@@ -4,30 +4,54 @@
 #include <map>
 #include <variant>
 #include <vector>
-
-
 class CJsonValue {
 
 private:
-  std::variant<std::nullptr_t, int, double, bool, std::string,
-               std::map<std::string, CJsonValue>, std::vector<CJsonValue>>
-      m_values;
+  using JsonObject = std::map<std::string, CJsonValue>;
+  using JsonArray = std::vector<CJsonValue>;
 
+  std::variant<std::nullptr_t, int, double, bool, std::string, JsonObject,
+               JsonArray>
+      m_value;
 
 public:
   CJsonValue() = default;
-  CJsonValue(int v);
-  CJsonValue(double v);
-  CJsonValue(bool v);
-  CJsonValue(const std::string &v);
-  
-  bool is_object() const;
-  bool is_array() const;
-  bool is_string() const;
-  bool is_number() const;
-  bool is_bool() const;
+  CJsonValue(int v) : m_value(v) {}
+  CJsonValue(double v) : m_value(v){}
+  CJsonValue(bool v) : m_value(v){}
+  CJsonValue(const std::string &v) : m_value(v){}
+  CJsonValue(const char *v) : m_value(std::string(v)){}
 
-  CJsonValue &operator[](std::string key);
-  CJsonValue &operator[](int index);
+  bool is_object() const { return m_value.index() == 5; }
+
+  bool is_null() const { return m_value.index() == 0; }
+
+  bool is_array() const { return m_value.index() == 6; }
+
+  bool is_string() const { return m_value.index() == 4; }
+  
+  bool is_number() const {
+    return m_value.index() == 1 || m_value.index() == 2;
+  }
+
+  bool is_bool() const { return m_value.index() == 3; }
+
+  CJsonValue &operator[](std::string key) {
+    if (!is_object()) {
+      m_value = JsonObject{};
+    }
+
+    auto &obj = std::get<JsonObject>(m_value);
+    return obj[key];
+  }
+
+  CJsonValue &operator[](int index) {
+    if (!is_array()) {
+      m_value = JsonArray{};
+    }
+
+    auto &arr = std::get<JsonArray>(m_value);
+    return arr[index];
+  }
   
 };
